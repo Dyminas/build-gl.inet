0d646ba4eecc2bc82513ca2a4b8707a0b2a3db1d
纯天然咸鱼 <Dyminas@outlook.com>
2022-08-08 13:17:48 +0800

    First Commit

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+CRTDIR=$(pwd)
+
+base=$1
+profile=$2
+ui=$3
+
+echo $base
+
+if [ ! -e "$base" ]; then
+	echo "Please enter base folder"
+	exit 1
+else
+	if [ ! -d $base ]; then 
+		echo "Openwrt base folder not exist"
+		exit 1
+	fi
+fi
+
+if [ ! -n "$profile" ]; then
+	profile=target_wlan_ap-gl-axt1800
+fi
+
+if [ ! -n "$ui" ]; then
+	ui=true
+fi
+
+
+echo "Start..."
+
+#clone source tree 
+git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
+cp -r *.yml $base/gl-infra-builder/profiles
+cd $base/gl-infra-builder
+
+#setup
+if [[ $profile == *5-4* ]]; then
+	python3 setup.py -c configs/config-wlan-ap-5.4.yml
+else
+	python3 setup.py -c configs/config-wlan-ap.yml
+fi
+
+
+cd wlan-ap/openwrt
+./scripts/gen_config.py $profile glinet_depends custom
+
+git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
+./scripts/feeds update -a 
+./scripts/feeds install -a
+make defconfig
+
+if [[ $ui == true  ]]; then 
+	make -j$(expr $(nproc) + 1) GL_PKGDIR=$base/glinet/ipq60xx/ V=s
+else
+	make -j$(expr $(nproc) + 1) V=s
+fi
